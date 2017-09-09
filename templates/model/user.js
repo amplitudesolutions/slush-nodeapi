093e638c47197f6d09b695dcb156e2c2fbed77e3
Martin Fedec <martin@amplitudesolutions.ca>
2017-09-08 22:47:30 -0700

    #5 completed

--- templates/model/user.js
+++ templates/model/user.js
@@ -4,7 +4,9 @@ var Schema = mongoose.Schema;
 var UserSchema = new Schema({
     name: String,
     email: String,
-    password: {type: String, select: false}
+    password: { type: String, select: false },
+    active: { type: Boolean, default: true },
+    deleted: { type: Boolean, select: false, default: false }
 });
 
 module.exports = mongoose.model("User", UserSchema);
